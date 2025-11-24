32a472eec992f9e80d36f0c7226c79cfb6821dd2
1024小神 <15670339118@163.com>
2025-11-24 22:15:32 +0800

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.pakeandroid.paketabs"
-    compileSdk = 36
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.pakeandroid.paketabs"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
